5c0382cd5957d1032ad2d5ab8b2d0491fb1f7af5
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-04-30 05:57:19 +0530

    data base connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+
+/* 
+    When we connect to the database we need to self remind 2 important things so that it's meets business requirement
+    1. The database is in another continent (means it will take time) so use Async await 
+    2. Write the code in the try&catch block for future remedy
+*/
 const connectDB = async() => {
     try {
        const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
