dc4fa28ba130829a2f5d585dac89cbfa404d3b56
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-05-05 20:01:40 +0530

    auth middleware added

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
-
+import { jwt } from "jsonwebtoken";
+import { User } from "../models/user.model";
 export const verifyJWT = asyncHandler(async(req,res,next)=>{
   const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
 
@@ -8,6 +9,19 @@ export const verifyJWT = asyncHandler(async(req,res,next)=>{
     throw new ApiError(401,"Unauthorised request")
   }
 
+  const decodedToken =  await jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
   
+  if(!decodedToken){
+    throw new ApiError(500,"Token not generated")
+  }
 
+const user =  await User.findById(decodedToken?._id)
+// .select("-password -refreshToken")
+
+  if(!user){
+    throw new ApiError(500,"Invalid Access Token")
+  }
+
+  req.user = user;
+  next();
 })
