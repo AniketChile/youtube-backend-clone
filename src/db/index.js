2cfc5f2f328fbadf4ed172d9eab12225e6bc167f
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-05-16 05:55:50 +0530

    database bug fixing

--- src/db/index.js
+++ src/db/index.js
@@ -1,21 +1,16 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import {DB_NAME} from "../constants.js"
 
-
-/* 
-    When we connect to the database we need to self remind 2 important things so that it's meets business requirement
-    1. The database is in another continent (means it will take time) so use Async await 
-    2. Write the code in the try&catch block for future remedy
-*/
-const connectDB = async() => {
+const connectDB = async ()=>{
     try {
-       const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDb connected !! Db host: ${connectionInstance.connection.host}`);
-    
+        const MONGO_URI = await process.env.MONGODB_URI;
+        console.log(MONGO_URI);
+        const connection =  await mongoose.connect(`${MONGO_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST : ${connection.connection.host}`);
     } catch (error) {
-        console.log("MONGODB connection error",error);
-        process.exit(1)
+        console.log("MongoDB connection error:"+error);
+        process.exit(1);
     }
 }
 
-export default connectDB;
+export default connectDB
