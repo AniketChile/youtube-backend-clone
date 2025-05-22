b4f3cb99c34c228513aa010534e6fbb51b8f85ec
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-05-22 06:26:24 +0530

    channel's subscriber and subsribe to cont added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -339,6 +339,71 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
 
 })
 
+const userChannelProfile = asyncHandler(async(req,res)=>{
+
+  const username = req.params;
+
+  if(!username?.trim()) throw new ApiError(400,"Enter the user name")
+
+  const channel = await User.aggregate([
+    {
+      $match:{
+        username : username?.toLowerCase()
+      }
+    },{
+      $lookup:{
+        from: "subscriptions",
+        localField:"_id",
+        foreignField: "channel",
+        as:"subsribers"
+      }
+    },{
+      $lookup:{
+        from: "subscriptions",
+        localField:"_id",
+        foreignField: "subscriber",
+        as:"channelSubscribeTo"
+      }
+    },{
+      $addFields:{
+        subcriberCount:{
+          $size:"$subsribers" 
+        },
+        channelSubscribeTo:{
+          $size: "$channelSubscribeTo"
+        },
+        isSubscribed:{
+          if: {
+            $in:[req.user?._id,"$subsribers.subscriber"]
+          },
+          then:true,
+          else:false,
+        }
+      }
+    },{
+      $project:{
+        fullName:1,
+        username:1,
+        subcriberCount:1,
+        channelSubscribeTo:1,
+        isSubscribed:1,
+        avatar:1,
+        coverImage:1,
+        email:1
+      }
+    }
+  ])
+
+  if(channel?.length){
+    throw new ApiError(404,"channel doesnot exist")
+  }
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200,channel[0],"User channel fetched succesfully"))
+
+})
+
 export {
   registerUser,
   loginUser,
